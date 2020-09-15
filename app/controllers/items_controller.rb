9702f817df312cd8345761642e6eb47c85c3f1dd
o4410o <itukusima1025@gmail.com>
2020-09-15 13:57:47 +0900

    rubocop済み

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,12 +2,12 @@ class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-
   end
-  
+
   def new
     @item = Item.new
   end
+
   def move_to_index
     redirect_to action: :index unless user_signed_in?
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-      with_options presence: true do
-         validates :nickname
-         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates :password, format:{with: PASSWORD_REGEX}
+  with_options presence: true do
+    validates :nickname
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates :password, format: { with: PASSWORD_REGEX }
 
-         with_options format: { with:/\A[ぁ-んァ-ン一-龥]+\z/} do
-           validates :kanji_family_name 
-           validates :kanji_first_name
-         end
-         with_options format: { with:/\A[ぁ-んァ-ン]+\z/,} do
-         validates :kana_family_name 
-         validates :kana_first_name 
-         end
+    with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do
+      validates :kanji_family_name
+      validates :kanji_first_name
+    end
+    with_options format: { with: /\A[ぁ-んァ-ン]+\z/ } do
+      validates :kana_family_name
+      validates :kana_first_name
+    end
 
-         validates :birthday
-      end
+    validates :birthday
+  end
 end
