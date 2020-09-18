21f3250a1b508eca2f62327a153d487c0d9d8ee5
o4410o <itukusima1025@gmail.com>
2020-09-18 15:24:23 +0900

    rubocopの実行

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe Item, type: :model do
   before do
     @item = FactoryBot.build(:item)
-    @item.item_image = fixture_file_upload("/files/hero.jpg")
+    @item.item_image = fixture_file_upload('/files/hero.jpg')
   end
 
   describe '商品出品機能' do
@@ -27,42 +27,42 @@ RSpec.describe Item, type: :model do
       it 'genre_idが１の時' do
         @item.genre_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Genre must be other than 1")
+        expect(@item.errors.full_messages).to include('Genre must be other than 1')
       end
       it 'status_idが１の時' do
         @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Status must be other than 1")
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it 'delivery_idが１の時' do
         @item.delivery_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery must be other than 1')
       end
       it 'shipping_area_idが１の時' do
         @item.shipping_area_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping area must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping area must be other than 1')
       end
       it 'shipping_date_id' do
         @item.shipping_date_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping date must be other than 1")
+        expect(@item.errors.full_messages).to include('Shipping date must be other than 1')
       end
       it 'priceがからの時' do
         @item.price = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price can't be blank", "Price is not a number", "Price is not a number")
+        expect(@item.errors.full_messages).to include("Price can't be blank", 'Price is not a number', 'Price is not a number')
       end
       it 'priceが300より小さい時' do
         @item.price = 200
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが9999999より大きい時' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
     end
   end
