a1d5a79089367341523c14106fced16d41f9d686
o4410o <itukusima1025@gmail.com>
2020-09-28 20:23:18 +0900

    ルボコップ

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -3,8 +3,8 @@ FactoryBot.define do
     token          { 'a' }
     post_code      { '123-4567' }
     prefecture_id  { Faker::Number.within(range: 2..48) }
-    city           { "国分寺市" }
-    street_number  { "4-4-4" }
-    phone_number   { "00000000000" }
+    city           { '国分寺市' }
+    street_number  { '4-4-4' }
+    phone_number   { '00000000000' }
   end
 end
